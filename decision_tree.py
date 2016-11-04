3a885550c4b165bd839d2ebbdcfb3a3e151dfd85
Marco Lopez <marcolm485@gmail.com>
2016-11-04 13:26:59 -0600

    Receive and format complete information

--- decision_tree.py
+++ decision_tree.py
@@ -1,7 +1,9 @@
 import numpy as np
+from pprint import pprint
 import sys
 
 matrix = []
+result = []
 relation = {}
 
 def get_instruction(input):
@@ -20,7 +22,6 @@ def get_relation(input):
 def get_data(input):
 	for (i, line) in enumerate(sys.stdin):
 		line = line.strip(" \n")
-		print(line)
 		if line is not "":
 			matrix.append([])
 			matrix[i] = line.split(",")
@@ -38,17 +39,27 @@ def get_attribute(input):
 		relation[attribute] = i
 
 def convert_matrix(matrix, relation):
+	new_matrix = []
 	for (i, row) in enumerate(matrix):
+		new_matrix.append([])
 		for (j, item) in enumerate(row):
-			#print(item)
-			#print(relation[item])
-			matrix[i][j] = relation[item]
-	return matrix
+			if j is len(row) - 1:
+				print(matrix[i][j])
+				#result.append(matrix[i][j])
+				result.append(relation[item])
+			else:
+				new_matrix[i].append(relation[item])
+	return new_matrix
+
 
 if __name__ == "__main__":
 	for line in sys.stdin:
 		get_instruction(line)
-	#print(matrix)
-	#print(relation)
 	new_matrix = convert_matrix(matrix, relation)
+	new_matrix = new_matrix[:len(new_matrix) - 1]
 	print(new_matrix)
+	print("Result")
+	print(result)
+	result = np.array(result)
+	new_matrix = np.array(new_matrix)
+	#print(recursive_split_function(new_matrix, result))
